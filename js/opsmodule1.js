a06d1da719f02a743099bfb36626bc294904173b
PCL <pclacademy@pcl.com.ph>
2024-10-16 12:56:47 +0800

    a

---
+++ js/opsmodule1.js
@@ -0,0 +1,22 @@
+let allVideos = [
+   {
+      name: "ASN Process (INBOUND)",
+      src: "./Module/ops/ASN-Process",
+      id: "vid_1"
+   },
+   {
+      name: "Buns Loadplanning Process (OUTBOUND)",
+      src: "./Module/ops/Buns-Loadplanning-Process",
+      id: "vid_2"
+   },
+   {
+      name: "GR Process (INBOUND)",
+      src: "./Module/ops/GR-Process",
+      id: "vid_3"
+   },
+   {
+      name: "RFUI Process (INBOUND)",
+      src: "./Module/ops/RFUI-Process",
+      id: "vid_4"
+   },
+ ]
