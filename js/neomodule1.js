cc968c26a3032aa6205de6e22a8fc4bc9a2d17a9
PCL <pclacademy@pcl.com.ph>
2025-03-27 15:22:36 +0800

    asdad

--- js/neomodule1.js
+++ js/neomodule1.js
@@ -9,4 +9,9 @@ let allVideos = [
       src: "./Module/neo/5S",
       id: "vid_2"
    },
+   {
+      name: "Good Warehousing Practices",
+      src: "./Module/neo/GWP",
+      id: "vid_3"
+   },
  ]
